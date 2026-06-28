4ca3aa8aa66760c7f5573fe1d70d6f7017d37f46
stepskop <skopek.stepan@gmail.com>
2026-06-28 22:22:50 +0200

    feat: add banner

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <signal.h>
+#include <iostream>
+#include "Banner.hpp"
 #include "EventLoop.hpp"
 
 int main() {
+    std::cout << SERVANT_BANNER << std::endl;
+
     // Ignore SIGPIPE. Piping to closed FD should not kill the server.
     signal(SIGPIPE, SIG_IGN);
 
